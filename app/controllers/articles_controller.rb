2ecd79a97ef60aeeb2bf8c4777f2d5d2a9009b4d
Evan Zeng <ewzeng99@gmail.com>
2021-03-10 00:26:18 -0500

    Added empty files

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/delete'
+  get 'articles/index'
+  get 'articles/show'
+
   resources :photos
   resources :proverbs
   resources :categories
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
